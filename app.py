f24c13dc9f3c5b6024bd87c585b063b837f5c174
s230256 <gimpo.s230256@ggh.goe.go.kr>
2024-11-22 14:08:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,23 +19,25 @@ st.write(' - 인공지능 모델 정확도 : ')
 col1, col2, col3 = st.columns(2)  
 with col1:
       st.subheader('데이터시각화1')
-      st.image('시각화1.png' )   # 이미지 불러오기
+      st.image('시각화8.png' )   # 이미지 불러오기
 with col2:
       st.subheader('데이터시각화2')
-      st.image('시각화2.png' )   # 이미지 불러오기
+      st.image('___9.png' )   # 이미지 불러오기
 
 # 4.모델 활용
 st.subheader('모델 활용')
 st.write('**** 다음을 입력하세요.. 인공지능이 당신의 수면의 질을 알려드립니다! ')
 st.write('**** 당신이 입력한 데이터는 다른 목적으로 절대 사용되지 않습니다. ')
 
-a = st.number_input(' 수강과목수 입력 ', value=0)      #초기값은 0
-b = st.number_input(' 공부시간 입력 ', value=0.0 )     # 초기값은 0.0
-c = st.selectbox('공지확인 입력(확인한다:0, 확인하지않는다:1', [0,1])
-                                                            # 사용자가  0,1 중에 선택
+a = st.number_input(' 나이 입력 ', value=0)      #초기값은 0
+b = st.number_input(' 평균 수면 시간 입력 ', value=0.0 )     # 초기값은 0.0
+c = st.number_input(' 평균 공부 시간 입력 ', value=0.0 )     # 초기값은 0.0
+d = st.number_input(' 전자기기 사용시간 입력 ', value=0)      #초기값은 0
+e = st.number_input(' 카페인 섭취량 입력 ', value=0.0 )     # 초기값은 0.0
+f = st.number_input(' 신체 활동량 입력 ', value=0.0 )     # 초기값은 0.0
 
-if st.button('점수예측'):            # 사용자가 '점수예측' 버튼을 누르면
-        input_data = [[a,b,c]]     # 사용자가 입력한 a,b,c 를 input_data에 저장하고
+if st.button('결과 확인하기'):            # 사용자가 '점수예측' 버튼을 누르면
+        input_data = [[a,b,c,d,e,f]]     # 사용자가 입력한 a,b,c 를 input_data에 저장하고
         p = model.predict(input_data)         # model이 예측한 값을 p에 저장한다
-        st.write('인공지능의 예측 점수는', p)
+        st.write('단신의 수면의 질은', p, '입니다.')
 
